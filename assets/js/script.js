dcf9f52799ba780a5ab4d3c550148a7c87746058
Thiago <matheusthiago822@gmail.com>
2023-01-19 20:35:55 -0300

    script para validação de checkbox (checkbox validation)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-console.log('Ola mundo');
+function validateForm (form) {
+	
+	if ((!form.op1.checked) && (!form.op2.checked) && (!form.op3.checked) && (!form.op4.checked)) {
+		document.getElementsByClassName("errorMessageForm")[0].style.visibility = "visible";
+		return false;
+	} else {
+		return true;
+	}
+}
