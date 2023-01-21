20c5dbb9fed69d6f58ec7fe324bfc5d2a43074cf
Thiago <matheusthiago822@gmail.com>
2023-01-21 18:22:21 -0300

    criacao de funcao para validar form checkbox, select, hover da index.html, alert de sucesso

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,45 @@
+//Validar o select da 3rd.html
+function validateSelect() {
+	var ddl = document.getElementById("select_account");
+	var selected = ddl.options[ddl.selectedIndex].value;
+	if (selected == "blank") {
+		document.getElementsByClassName("errorMessageForm")[0].style.visibility = "visible";
+		document.getElementsByClassName("select3rd")[0].style.backgroundColor = "#AEAEAE";
+		
+		setTimeout(() => {
+			document.getElementsByClassName("select3rd")[0].style.backgroundColor = "";
+		}, 450);
+		
+		return false;
+	} else {
+		return true;
+}
+}
+
+// Validar o checkbox da 4th.html
 function validateForm (form) {
-	
 	if ((!form.op1.checked) && (!form.op2.checked) && (!form.op3.checked) && (!form.op4.checked)) {
 		document.getElementsByClassName("errorMessageForm")[0].style.visibility = "visible";
 		return false;
 	} else {
 		return true;
 	}
+}
+
+//Hover do botao da index
+function hover() {
+	
+	event.target.style.backgroundColor = "#FFFF7D";
+	event.target.innerHTML = "Just Go Ahead";
+	event.target.style.color = "black";
+}
+
+//Mouse fora do elemento
+function fora() {
+		event.target.style.backgroundColor = "";
+		event.target.innerHTML = "";
+}
+
+function successful () {
+	alert ("Seu cadastro foi completo. Muito obrigado :D");
 }
