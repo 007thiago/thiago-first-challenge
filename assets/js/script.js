f0174b0407118183a92ae6f369dc7c819156b1f9
Thiago <matheusthiago822@gmail.com>
2023-01-23 13:48:14 -0300

    adicionada funcao para pagina 4th.html, permitindo o Next apenas se o select e checkbox estiverem marcados

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-//Validar o select da 3rd.html
-function validateSelect() {
+// Validar o select da 3rd.html
+function validateSelect (form) {
 	var ddl = document.getElementById("select_account");
 	var selected = ddl.options[ddl.selectedIndex].value;
 	if (selected == "blank") {
@@ -13,18 +13,35 @@ function validateSelect() {
 		return false;
 	} else {
 		return true;
-}
+	}
 }
 
-// Validar o checkbox da 4th.html
+
+// Validar o formulario da 4th.html
 function validateForm (form) {
-	if ((!form.op1.checked) && (!form.op2.checked) && (!form.op3.checked) && (!form.op4.checked)) {
-		document.getElementsByClassName("errorMessageForm")[0].style.visibility = "visible";
+	var ddl = document.getElementById("select_account");
+	var selected = ddl.options[ddl.selectedIndex].value;
+	if (selected == "blank") {
+		document.getElementsByClassName("errorMessageForm1")[0].style.visibility = "visible";
+		document.getElementsByClassName("select3rd")[0].style.backgroundColor = "#AEAEAE";
+		
+		setTimeout(() => {
+			document.getElementsByClassName("select3rd")[0].style.backgroundColor = "";
+		}, 450);
+		return false;
+	} else {
+		if ((!form.op1.checked) && (!form.op2.checked) && (!form.op3.checked) && (!form.op4.checked)) {
+		document.getElementsByClassName("errorMessageForm2")[0].style.visibility = "visible";
+		document.getElementsByClassName("errorMessageForm1")[0].style.visibility = "hidden";
 		return false;
 	} else {
 		return true;
 	}
+	}
+	
+	
 }
+
 
 //Hover do botao da index
 function hover() {
@@ -33,6 +50,7 @@ function hover() {
 	event.target.innerHTML = "Just Go Ahead";
 	event.target.style.color = "black";
 }
+
 
 //Mouse fora do elemento
 function fora() {
